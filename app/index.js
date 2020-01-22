06e27a746c2b5d87d338821e9cad1f85ce9f89d8
Carl Roloff <cgr6@case.edu>
2020-01-22 16:45:36 -0500

    Added css

--- app/index.js
+++ app/index.js
@@ -55,30 +55,34 @@ class App extends React.Component{
 			before: addDays(new Date(), 3),
 		}
         return(
-            <div>
-				<DayPicker
-					onDayClick={this.handleDayClick}
-					selectedDays={this.state.selectedDay}
-					disabledDays={[past, { daysOfWeek: [0, 6] }]}
-				/>
-				<div className="tour-list">{this.state.loaded && (this.state.tours.constructor === Array) ? (
-					<div><h3>You selected {this.state.selectedDay.toLocaleDateString()}</h3>
-						Tours available
-						{this.state.tours.map((tour, i)=>{
-							return(
-								<div key={i.toString()}>
-									<a href={"https://case.edu/law/admissions/jd-admissions/visit-us/schedule-visit/tour-registration?date=" + dateFnsFormat(parseISO(tour.start.dateTime), 'MMM. d yyyy h:mm a') + "&class=" + tour.summary}>{tour.summary}: {dateFnsFormat(parseISO(tour.start.dateTime), 'h:mm a')} - {dateFnsFormat(parseISO(tour.start.dateTime), 'h:mm a')}</a>
-								</div>
-							)
-						})}
+            <div class="container-fluid">
+				<div class="row no-margin">
+					<div class="col-xs-12 col-sm-6 calendar-column">
+						<DayPicker
+							onDayClick={this.handleDayClick}
+							selectedDays={this.state.selectedDay}
+							disabledDays={[past, { daysOfWeek: [0, 6] }]}
+						/>
 					</div>
-				) : (
-					(this.state.loaded ? (
-						<div><h3>There are no tours available on {this.state.selectedDay.toLocaleDateString()}. Please select a different date.</h3></div>
+					<div className="col-xs-12 col-sm-6 tour-list">{this.state.loaded && (this.state.tours.constructor === Array) ? (
+						<div><h3>You selected {dateFnsFormat(this.state.selectedDay, 'MMM d, yyyy')}</h3>
+							Tours available
+							{this.state.tours.map((tour, i)=>{
+								return(
+									<div className="tour" key={i.toString()}>
+										<a href={"https://case.edu/law/admissions/jd-admissions/visit-us/schedule-visit/tour-registration?date=" + dateFnsFormat(parseISO(tour.start.dateTime), 'MMM. d yyyy h:mm a') + "&class=" + tour.summary}>{tour.summary}: {dateFnsFormat(parseISO(tour.start.dateTime), 'h:mm a')} - {dateFnsFormat(parseISO(tour.end.dateTime), 'h:mm a')}</a>
+									</div>
+								)
+							})}
+						</div>
 					) : (
-						<p>Please select a day.</p>
-					)
-				))}</div>
+						(this.state.loaded ? (
+							<div><h3>There are no tours available on {dateFnsFormat(this.state.selectedDay, 'MMM d, yyyy')}. Please select a different date.</h3></div>
+						) : (
+							<p>Please select a day.</p>
+						)
+					))}</div>
+				</div>
 			</div>
         )
     }
